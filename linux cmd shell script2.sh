448be10b29f741ef45f0f7ddceb1302477eaae67
yashwanth <yy182460gmail.com>
2026-06-25 20:59:51 +0530

    shell scrpit 2

---
+++ linux cmd shell script2.sh
@@ -0,0 +1,27 @@
+ sudo su -
+ sudo dnf update
+ sudo dnf upgrade
+ clear
+ ls -lrt
+ touch file1.sh file2.sh file3.sh
+ ls
+ mkdir dir1 dir2 dir3
+ ls -lrt
+ nano file1.sh
+ cat file1.sh
+ :%s/Jun/Dec/g
+ clear
+ :set nu
+ Vi file1.sh
+ vi file1.sh
+ cat file1.sh
+ cat file2.sh
+ cp file1.sh file2.sh
+ cat file2.sh
+ cp file1.sh /dir1
+ sudo cp file1.sh dir1
+ cd dir1
+ ls
+ cd ..
+ ls
+ histroy
