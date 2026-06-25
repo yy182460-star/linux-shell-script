bf79631bd14f9bce7e638accdebf79d88ae8aa7f
yashwanth <yy182460gmail.com>
2026-06-25 20:10:00 +0530

    linux shell script

---
+++ linux command shell 1.sh
@@ -0,0 +1,26 @@
+  sudo -i
+  clear
+     touch simha.sh
+     ls
+     ls -l
+     mkdir project
+      ls
+      ls -l
+      ls -lrt
+     pwd
+     cd project
+     mkdir miniproject
+     cd miniproject
+     pwd
+     cd ..
+     pwd
+     cd ../..
+     pwd
+     ls
+     cd ..
+     ls
+     cd home/ec2-user
+     ls
+     vi simha.sh
+  cat simha.sh
+   history
