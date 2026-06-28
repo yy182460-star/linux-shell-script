36477240a6f9464ab44a711d35fa03f1110c63a7
yashwanth <yy182460gmail.com>
2026-06-28 19:45:57 +0530

    shell script

---
+++ linux cmd shell script4.sh
@@ -0,0 +1,64 @@
+    1  sudo dnf update
+    2  sudo yum upgrade
+    3  clear
+    4  ls
+    5  mkdir floder1 floder2
+    6  touch file.sh file1.sh
+    7  ls
+    8  cp file.sh /floder2
+    9  ls -l
+   10  sudo cp file.sh /floder2
+   11  cd fioder2
+   12  cd floder2
+   13  ls
+   14  cd ..
+   15  claer
+   16  clear
+   17  ls
+   18  mv file.sh file2.sh
+   19  ls
+   20  mv file2.sh floder2
+   21  ls
+   22  cd floder2
+   23  ls
+   24  cd ..
+   25  ls
+   26  mv * floder2
+   27  cd folder2
+   28  cd floder2
+   29  mkdir floder2.1 floder2.2 floder2.3
+   30  touch file2.sh file3.sh
+   31  cd floder2.1
+   32  mkdir floder2.1.1 floder2.1.2
+   33  tree
+   34  cd ../..
+   35  pwd
+   36  sudo dnf install tree
+   37  tree
+   38  pwd
+   39  cd ../..
+   40  tree
+   41  clear
+   42  ls 
+   43  df -h
+   44  df -h .
+   45  du -sh floder2
+   46  touch file
+   47  du -sh file
+   48  du -sh *
+   49  free -h
+   50  echo "hello from linux"
+   51  echo "hello \n from linux"
+   52  echo -e "hello \n from linux"
+   54  clear
+   55  history
+   56  ls
+   57  grep "^$" file
+   58  vi file
+   59  grep "^$" file
+   60  grep -c "^$" file
+   61  grep -v "^$" file 
+   63  grep -v "^$" file > file2
+   64  ls
+   65  cat file2
+   66  history
