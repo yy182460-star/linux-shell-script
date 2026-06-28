f181e78420421e0726be2ab47b2b8da21078f4aa
yashwanth <yy182460gmail.com>
2026-06-28 19:39:30 +0530

    shell script

---
+++ linux cmd shell scrpit3.sh
@@ -0,0 +1,25 @@
+sudo apt-get update
+  sudo apt-get upgrade -y
+   clear
+  df -h
+   lsblk
+   clear
+   df -h
+   lsblk
+  clear
+  file -s /dev/xvdbz
+  mkfs -t xfs /dev/xvdbz
+  file -s /dev/xvdbz
+  lsblk
+  mkdir /apps
+  ls
+  cd apps
+  \mkdir -p /apps
+  mkdir -p /apps
+  cd /apps
+  mount /dev/xvdbz /apps
+  clear
+  lsblk
+  df -h
+  clear
+  history
